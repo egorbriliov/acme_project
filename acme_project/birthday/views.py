423984ec26bb433c859aecd31697494644ef6a58
Egor Shashkun <stas.basov@whatever.com>
2026-03-26 11:12:01 +0300

    Добавил новую страницу для отображения информации. Кроме того минимизировал количества кода

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,29 +1,40 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+     CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-
-
-class BirthdayMixin:
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    form_class = BirthdayForm
-
-
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    form_class = BirthdayForm
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
+
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
