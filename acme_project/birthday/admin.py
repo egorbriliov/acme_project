c55e959e3757f5112285687e9d1e216c62f592cb
Egor Shashkun <stas.basov@whatever.com>
2026-03-27 04:15:46 +0300

    Добавил в админку управлене данными о днях рождения

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,28 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+        'image'
+        )
+    list_editable = (
+        'last_name',
+        'birthday',
+        'image'
+    )
+    search_fields = (
+        'first_name',
+        'last_name'
+    )
+    list_filter = (
+        'birthday',
+    )
+    list_display_links = (
+        'first_name',
+    )
