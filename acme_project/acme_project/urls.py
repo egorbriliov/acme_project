60d3e057a7a607f544b30ee8781115b4934deb86
Egor Shashkun <stas.basov@whatever.com>
2026-03-28 11:12:09 +0300

    Заменил предыдущую форму регистрации на новую, с заполнением bio.

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
+
+from users.forms import CustomUserCreationForm
 
 
 urlpatterns = [
@@ -15,7 +16,7 @@ urlpatterns = [
         'auth/registration/',
         CreateView.as_view(
             template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
+            form_class=CustomUserCreationForm,
             success_url=reverse_lazy('pages:homepage')
         ),
         name='registration'
