09e0a894761d666af50adbe929492ca17ad24d75
Egor Shashkun <stas.basov@whatever.com>
2026-03-28 09:59:58 +0300

    Добил переадрисацию после входа на главную страницу. Заменил стандартный Django адрес для входа на собственный.

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -15,6 +15,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # Указываем директорию, в которую будут сохраняться файлы писем:
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -97,5 +101,3 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
